323777d1c0c9ccd41b638c8db5b4d7e57e6f645f
Fabrício Herpich <fabricio.herpich@gmail.com>
2021-10-20 21:41:47 -0300

    Enviando os arquivos do projeto de agenda de contatos

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:agendadecontatos/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+
+
+/**
+
+url_launcher: ^5.0.2
+image_picker: ^0.6.0+3
+ */
